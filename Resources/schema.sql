5169a2669f1643365957f6c85cf9272ed56f49c2
jason-hernandez-73 <56489096+jason-hernandez-73@users.noreply.github.com>
2020-03-28 14:10:22 -0700

    Create schema.sql

---
+++ Resources/schema.sql
@@ -0,0 +1,11 @@
+
+
+CREATE TABLE Covid19(
+	states VARCHAR (35) NOT NULL,
+	covid_confirmed_cases INT,
+	covid_confirmed_deaths FLOAT);
+	
+CREATE TABLE H1N1(
+	states VARCHAR (35) NOT NULL,
+	h1n1_confirmed_cases INT,
+	h1n1_confirmed_deaths INT);
