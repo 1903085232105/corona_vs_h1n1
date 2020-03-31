c2fc32b14cd1803d0200d6f5d6d7f595e743fbf9
jason-hernandez-73 <56489096+jason-hernandez-73@users.noreply.github.com>
2020-03-30 18:41:07 -0700

    Update schema.sql

--- Resources/schema.sql
+++ Resources/schema.sql
@@ -9,3 +9,14 @@ CREATE TABLE H1N1(
 	states VARCHAR (35) NOT NULL,
 	h1n1_confirmed_cases INT,
 	h1n1_confirmed_deaths INT);
+
+SELECT
+	h1n1.states,
+	covid19.states,
+	h1n1.h1n1_confirmed_cases,
+	covid19.covid_confirmed_cases,
+	h1n1.h1n1_confirmed_deaths,
+	covid19.covid_confirmed_deaths
+FROM h1n1
+LEFT OUTER JOIN covid19 
+	ON h1n1.states=covid19.states;
